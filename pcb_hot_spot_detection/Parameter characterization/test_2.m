2d6e3df8cb795dcd71243ee88cc5315ceba669d3
Eladio Barrio Querol <47576771+ebarrio@users.noreply.github.com>
2020-01-19 12:00:00 +0100

    Add files via upload

--- pcb_hot_spot_detection/Parameter characterization/test_2.m
+++ pcb_hot_spot_detection/Parameter characterization/test_2.m
@@ -38,8 +38,9 @@ for i = 1:20
 end
 %% Erode + squeeze
 close all; clc
+H = ones(2); % For mean filter
 for j = 1:2
-    if (j == 1)
+    if (j == 2)
         ops_fig = figure(); ops_fig.WindowState = 'maximized';ops_fig.Name = 'Erode(20px) + squeeze image';
         se_erode_sqr = strel('square',5);
     else
@@ -47,7 +48,7 @@ for j = 1:2
         se_erode_sqr = strel('square',30);
     end
     
-    N_R = 2; N_C = 5; i = 1; img_trg = 180;img_trg2 = 215;
+    N_R = 2; N_C = 6; i = 1; img_trg = 180;img_trg2 = 215;
     Ies = imerode(hot,se_erode_sqr);
     Ies = squeeze(Ies(:,:,1)); % Ralza pixeles oscuros en zonas luminosas
     subplot(N_R , N_C , i),imagesc(hot);i = i+1;
@@ -55,6 +56,8 @@ for j = 1:2
     subplot(N_R , N_C , i),imagesc(Ies>110);i = i+1;
     subplot(N_R , N_C , i),imagesc(Ies>img_trg);i = i+1;
     subplot(N_R , N_C , i),imagesc(Ies>img_trg2);i = i+1;
+    S = imfilter(Ies,H);
+    subplot(N_R , N_C,i),imagesc(S>img_trg2);i = i+1;
     Ies = imerode(cold,se_erode_sqr);
     Ies = squeeze(Ies(:,:,1)); % Ralza pixeles oscuros en zonas luminosas
     subplot(N_R , N_C,i),imagesc(cold);i = i+1;
@@ -62,4 +65,7 @@ for j = 1:2
     subplot(N_R , N_C,i),imagesc(Ies>110);i = i+1;
     subplot(N_R , N_C,i),imagesc(Ies>img_trg);i = i+1;
     subplot(N_R , N_C,i),imagesc(Ies>img_trg2);i = i+1;
+    S = imfilter(Ies>img_trg,H);
+    subplot(N_R , N_C,i),imagesc(S);i = i+1;
+    
 end
